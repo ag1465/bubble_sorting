0a7f5d916471ff1d20212dafed48b19d174d1dfa
Allen Guo <ag1465@scarletmail.rutgers.edu>
2019-09-04 14:58:14 -0400

    initialized mergesort

--- bubble.sort.spec.js
+++ bubble.sort.spec.js
@@ -9,4 +9,10 @@ describe('Bubble Sort', function(){
     let testArr = [4, 78, 35, 20, 34, 10, 14, 23, 4, 56, 23, 3, 4, 23, 13, 1]
     expect( bubbleSort(testArr)).toEqual([1, 3, 4, 4, 4, 10, 13, 14, 20, 23, 23, 23, 34, 35, 56, 78]);
   })
+  // it('expect sort age', function (){
+  //   it('expect sort', function() {
+  //     let testArr = [{ age: 4 }, { age: 8 }, { age: 2 }, { age: 9 }]
+  //     expect( bubbleSort(testArr)).toEqual([{ age: 2 }, { age: 4 }, { age: 8 }, { age: 9 }]);
+  //   })
+  // })
 });
---
+++ mergesort.js
@@ -0,0 +1,11 @@
+function split(wholeArray) {
+
+  /* your code here to define the firstHalf and secondHalf arrays */
+
+  return [firstHalf, secondHalf];
+}
+function mergeSort(array) {
+
+  /* your code here */
+
+}
